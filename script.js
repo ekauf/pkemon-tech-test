3abb0266a1052c14cac4d48e8199970d632e60fd
Edgars <Edgars.Kaufmanis@nology.io>
2022-11-23 14:45:02 +0000

    added an outline to renderPokemon function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 import { pokemonArray } from "./data/pokemon.js";
 
-console.log(pokemonArray);
+const cardContainer = document.querySelector(".card-container");
+
+const renderPokemon = (pokemon) => {
+  let htmlString = "";
+
+  pokemonArray.forEach((pokemon) => {
+    console.log(pokemon);
+    htmlString += ` 
+    <section>
+        <img src="" alt=""></img>
+        <h1> </h1>
+        <p> </p>
+    </section>
+    `;
+  });
+
+  cardContainer.innerHTML = htmlString;
+};
+
+renderPokemon();
